8089e70c1f52e3bfe8a679779563d78b827701e2
yessine mkadem <yessinemkadem@yahoo.fr>
2022-10-10 11:20:03 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ComputerComponent } from './computer/computer.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LaptopComponent } from './laptop/laptop.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'laptop',component:LaptopComponent},
+  {path:'product',component:ProductComponent},
+  {path:'computer',component:ComputerComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
